84339d093cf12ecc2f5553ca459ac8c9ee1c2c52
Anthony Lin <anthonylin@MDG69V956P.tld>
2025-07-11 11:24:26 -0400

    added dictionary database

---
+++ app/models/dictionary.py
@@ -0,0 +1,113 @@
+import re
+from app.db import get_db
+from pypinyin import pinyin, Style
+
+# Optional: find a better pinyin converter and use pinyin from sql
+
+def get_search_results(field: str, query: str, limit: int = 50):
+    """
+    Retrieve search results from the dictionary based on the specified field and query.
+    Field must be one of 'simplified', 'pinyin_normalized', 'definitions', or 'id'.
+    Returns a list of dictionaries with 'id', 'simplified', 'pinyin', 'definitions'.
+    """
+    if field == "id":
+        return [get_word_by_id(int(query))]
+    else:
+        sql = f"""
+            SELECT id, simplified, definitions
+            FROM dictionary
+            WHERE {field} %% %s
+            ORDER BY SIMILARITY({field}, %s) DESC
+            LIMIT %s;
+            """
+        params = (query, query, limit)
+    conn = get_db()
+    cursor = conn.cursor()
+
+    cursor.execute(sql, params)
+    rows = cursor.fetchall()
+    results = []
+    for row in rows:
+        id, simplified, definitions = row
+        pinyin_ = ''.join(syllable[0] for syllable in pinyin(simplified, style=Style.TONE))
+        word = {"id": id,
+                "simplified": simplified,
+                "pinyin": pinyin_,
+                "definitions": definitions.split("/")[1:-1]}
+        results.append(word)
+
+    cursor.close()
+    conn.close()
+
+    return results
+
+
+def get_word_by_id(id: int):
+    """
+    Retrieve a word's detail from the dictionary.
+    Returns a dictionary with 'id', 'simplified', 'pinyin', 'definitions'.
+    """
+    conn = get_db()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT simplified, definitions FROM dictionary WHERE id = %s", (id,))
+    row = cursor.fetchone()
+
+    if row:
+        simplified = row[0]
+        pinyin_ = ''.join(syllable[0] for syllable in pinyin(simplified, style=Style.TONE))
+        definitions = row[1].split("/")[1:-1]
+        result = {"id": id, 
+                  "simplified": simplified,
+                  "pinyin": pinyin_,
+                  "definitions": definitions}
+        cursor.close()
+        conn.close()
+        return result
+
+    cursor.close()
+    conn.close()
+    return None
+
+
+if __name__ == "__main__":
+    conn = get_db()
+    cursor = conn.cursor()
+    batch_size = 1000
+    batch = []
+
+    with open("/Users/anthonylin/Downloads/cedict_ts.u8", encoding="utf-8") as f:
+        for line in f:
+            if line.startswith("#"):
+                continue
+
+            match = re.match(r"(\S+)\s+(\S+)\s+\[(.+?)\]\s+/(.+)/", line)
+            if not match:
+                continue
+
+            trad, simp, pinyin, defs = match.groups()
+            definitions = "/" + defs.strip("/") + "/"
+            batch.append((simp, pinyin, definitions))
+            if len(batch) >= batch_size:
+                cursor.executemany(
+                    "INSERT INTO dictionary (simplified, pinyin, definitions) VALUES (%s, %s, %s)",
+                    batch
+                )
+                print(f"Inserted {len(batch)} records into the database.")
+                batch.clear()
+                conn.commit()
+                
+        
+        if batch:
+            cursor.executemany(
+                "INSERT INTO dictionary (simplified, pinyin, definitions) VALUES (%s, %s, %s)",
+                batch
+            )
+            conn.commit()
+            batch.clear()
+            
+
+
+    cursor.close()
+    conn.close()
+    print("Dictionary loaded successfully.")
