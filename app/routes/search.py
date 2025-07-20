333049e860526fb0be27266e559b2867ae3c57be
Anthony Lin <anthonylin@MDG69V956P.tld>
2025-07-19 23:40:01 -0400

    updated search.py as backend

--- app/routes/search.py
+++ app/routes/search.py
@@ -1,20 +1,24 @@
-from flask import Blueprint, render_template, request, redirect, url_for, jsonify
-from flask_login import login_required, current_user
+from flask import Blueprint, request, jsonify
 from app.models.dictionary import get_search_results
-from app.models.vocab_bank import get_vocab, get_all_vocab, add_vocab, update_vocab
+from app.models.vocab_bank import get_vocab, get_all_vocab, add_vocab, update_vocab, get_random_vocab
+from flask_jwt_extended import jwt_required
 
-bp = Blueprint('search', __name__)    
+bp = Blueprint('search', __name__, url_prefix='/api')  
+
 # Perhaps caching???
-@bp.route('/search/<search_type>/<query>')
-@login_required
+@bp.route('/search/<search_type>/<query>', methods=['POST'])
+@jwt_required()
 def search_query(search_type, query):
     """Search for a query in the dictionary and render results."""
     KEY = {"simplified": "simplified",
            "pinyin": "pinyin_normalized",
            "definitions": "definitions",
            "id": "id"}
+    data = request.get_json()
+    user = data.get('user', '')
+
     results = get_search_results(KEY[search_type], query)
-    all_vocab = get_all_vocab(current_user.id)
+    all_vocab = get_all_vocab(user['id'])
     # Convert all_vocab to a set for faster lookup
     all_vocab_set = {vocab["simplified_id"] for vocab in all_vocab}
 
@@ -22,11 +26,21 @@ def search_query(search_type, query):
         # Fetch is_added status
         result["is_added"] = True if result["id"] in all_vocab_set else False
 
-    return render_template('search.html', user=current_user, results=results)
+    return jsonify(results)
+
+@bp.route('/search/recommended', methods=['POST'])
+@jwt_required()
+def search_recommended():
+    data = request.get_json()
+    user = data.get('user', '')
+    results = get_random_vocab(user["id"], 30)
+    for result in results:
+        result["is_added"] = False
+    return jsonify(results)
 
 
 @bp.route('/search', methods=["GET"])
-@login_required
+@jwt_required()
 def search():
     """
     Search for a word in the dictionary. The search can be based on:
