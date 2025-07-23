40c4c6731138e005a9d7bf01eaa2aa8b6a8a83b1
Anthony Lin <anthonylin@MDG69V956P.tld>
2025-07-23 19:09:39 -0400

    changed id to sentence_id

--- app/models/sentences.py
+++ app/models/sentences.py
@@ -1,11 +1,10 @@
 from app.db import get_db
-from pypinyin import pinyin, Style
 
 
-def get_sentences_by_ids(sentence_ids: list[int]):
+def get_sentences_by_ids(sentence_ids: list[int]) -> dict:
     """
     Retrieve sentences by their IDs. Returns a dictionary of sentences (key=id), where each sentence
-    is represented as a dictionary with 'id', 'chinese', 'pinyin', and 'english'.
+    is represented as a dictionary with 'sentence_id', 'chinese', 'pinyin', and 'english'.
     """
     if not sentence_ids:
         return {}
@@ -14,14 +13,14 @@ def get_sentences_by_ids(sentence_ids: list[int]):
     cursor = conn.cursor()
 
     cursor.execute(
-        "SELECT id, chinese, english, pinyin FROM sentences WHERE id = ANY(%s)",
+        "SELECT sentence_id, chinese, english, pinyin FROM sentences WHERE sentence_id = ANY(%s)",
         (sentence_ids,)
     )
     
     results = cursor.fetchall()
     sentences = {}
     for row in results:
-        sentences[row[0]] =  {'id': row[0],
+        sentences[row[0]] =  {'sentence_id': row[0],
                               'chinese': row[1], 
                               'pinyin': row[3],
                               'english': row[2]}
