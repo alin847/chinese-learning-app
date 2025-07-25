7d1ac70a9ef528fc08d49563f875070ae2303083
Anthony Lin <anthonylin@MDG69V956P.tld>
2025-07-25 09:20:07 -0400

    added progress page

---
+++ frontend/src/pages/Progress.jsx
@@ -0,0 +1,95 @@
+import HeaderHome from "../components/HeaderHome";
+import HomeSidebar from "../components/HomeSidebar";
+import { useState, useEffect } from "react";
+import Loading from "../components/Loading";
+import './Progress.css';
+
+function Progress() {
+  // Types of data to show
+  // 1. # learning, # reviewing, # mastered words
+  // 2. bar chart of % of hsk words mastered per level
+  // 3. # of practice questions answered
+  // 4. # hrs spent practicing
+  const [learningCount, setLearningCount] = useState(0);
+  const [reviewingCount, setReviewingCount] = useState(0);
+  const [masteredCount, setMasteredCount] = useState(0);
+  const [practiceQuestionsAnswered, setPracticeQuestionsAnswered] = useState(0);
+  const [hoursSpentPracticing, setHoursSpentPracticing] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Fetch all vocab word for the counts
+    fetch('http://localhost:4000/api/progress/', {
+      method: 'GET',
+      headers: {
+        "Authorization": `Bearer ${localStorage.getItem('token')}`,
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Process data to get counts
+        setLearningCount(data.learning_count);
+        setReviewingCount(data.reviewing_count);
+        setMasteredCount(data.mastered_count);
+        setHoursSpentPracticing(Math.ceil(data.time_spent / 3600)); // Convert seconds to hours and ceil
+        setPracticeQuestionsAnswered(data.practice_completed);
+        setLoading(false);
+      })
+      .catch(err => {
+        console.error('Error fetching vocab data:', err);
+      });
+
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <div className="progress">
+      <HeaderHome />
+
+      <div className="home-container">
+        <HomeSidebar activePage="progress" />
+
+        <main className="content">
+          <h1>Progress</h1>
+
+          <div className="progress-container">
+            <div className="progress-circle">
+              <h2>Total Words</h2>
+              <p>{learningCount + reviewingCount + masteredCount}</p>
+            </div>
+
+            <div className="progress-circle">
+              <h2>Hours Spent Practicing</h2>
+              <p>{hoursSpentPracticing}</p>
+            </div>
+
+            <div className="progress-circle">
+              <h2>Words Learning</h2>
+              <p>{learningCount}</p>
+            </div>
+
+            <div className="progress-circle">
+              <h2>Words Reviewing</h2>
+              <p>{reviewingCount}</p>
+            </div>
+
+            <div className="progress-circle">
+              <h2>Words Mastered</h2>
+              <p>{masteredCount}</p>
+            </div>
+
+            <div className="progress-circle">
+              <h2>Practice Questions Answered</h2>
+              <p>{practiceQuestionsAnswered}</p>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default Progress;
