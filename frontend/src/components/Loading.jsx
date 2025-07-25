b4d565bb2317495721c6681535a85cc70abfce3e
Anthony Lin <anthonylin@MDG69V956P.tld>
2025-07-24 20:38:52 -0400

    added loading page

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,9 @@
+
+function Loading() {
+    return (
+        <div className="loading-container">
+            <div className="loading-spinner"></div>
+        </div>
+    );
+}
+export default Loading;
