5b535e5d841956aa00b2bf680331637426d0f182
Anthony Lin <anthonylin@MDG69V956P.tld>
2025-07-25 10:27:14 -0400

    fixed box-shadows

--- frontend/src/pages/Placement.jsx
+++ frontend/src/pages/Placement.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 import ProgressBar from '../components/ProgressBar';
+import "./Test.css";
 import "./Placement.css";
 
 function Placement() {
