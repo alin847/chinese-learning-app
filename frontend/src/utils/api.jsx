2fae860080f29746972d25a83596617ef4fda6af
Anthony Lin <anthonylin@MDG69V956P.tld>
2025-07-26 22:24:01 -0400

    fixed api.jsx

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -13,9 +13,6 @@ export const fetchAPI_JSON = async (endpoint, options = {}) => {
 
     const response = await fetch(`${API_BASE_URL}${endpoint}`, config);
 
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
     return response.json();
 };
 
@@ -32,8 +29,5 @@ export async function fetchAPI_BLOB(endpoint, options = {}) {
 
     const response = await fetch(`${API_BASE_URL}${endpoint}`, config);
 
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
     return response.blob();
 }
